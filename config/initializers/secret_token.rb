ae7a18595db70a7d10277bc8feaa81e34176712b
seuraul <kenny.is.inmortal@gmail.com>
2012-07-27 09:39:14 -0300

    blog2 create and dbuser

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog2::Application.config.secret_token = '47a695be690ee17a2b116851ca8989aeae186282ec24a32d93e7fc9d94ffc21d91ff89ccd3c4c2fae6caa02debb1bc19104307316632c16c9ec93df1b32b7d5b'
